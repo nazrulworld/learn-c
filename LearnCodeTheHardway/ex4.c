7676dc4e47b55880ad85219f6d8d015797ab6792
nazrul <mail2nazrul@yahoo.com>
2014-01-08 17:10:46 +0600

    Exersice upto 6

---
+++ LearnCodeTheHardway/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int subject_code = 909;
+    int total_mark = 100 ;
+    float obtain_mark;
+
+    printf("Examination's subject is %d.\n", subject_code);
+    printf("Total Mark: %d \n ", total_mark);
+    printf("Obtain Mark: %f \n", obtain_mark);
+
+    return 0;
+}
---
+++ LearnCodeTheHardway/ex5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    printf("Dhaka is %d kilometers distance form Tangail", distance);
+    return 0;
+}
